b9c4cce75d7005d4ef5c5d4ad46e423b8387bf7e
maf <maf@runtastic.com>
2014-05-07 07:21:14 +0200

    Added pingdom check returning xml, add pingdom to url

--- lib/middleware/health_monitor_middleware.rb
+++ lib/middleware/health_monitor_middleware.rb
@@ -10,11 +10,20 @@ class HealthMonitorMiddleware
   
   def call(env)
     if env["REQUEST_URI"] =~ /#{@uri}/
+
       request = Rack::Request.new(env)
-
+      
+      stime = Time.now
       data = @@monitor.get_status(request.params)
+      data[:time] = (Time.now - stime)
 
-      [200, { 'Content-Type' => 'application/json' }, [ data.to_json ]]
+      if env["REQUEST_URI"] =~ /pingdom/
+        status = (data[:status] == "up") ? "OK" : "FAILED"
+        xml_response = "<pingdom_http_custom_check><status>#{status}</status><response_time>#{data[:time]}</response_time></pingdom_http_custom_check>"
+        [200, { 'Content-Type' => 'application/xml' },  [ xml_response ]]
+      else
+        [200, { 'Content-Type' => 'application/json' }, [ data.to_json ]]
+      end
     else 
       @app.call(env)
     end
