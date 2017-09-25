eaa06fe6c788c77e47c47eaf175ce4c297b182fd
Dominik Goltermann <dominik.goltermann@runtastic.com>
2017-09-25 13:28:26 +0200

    rounding responsetime in pingdom xml to 3 digits do adhere to their standard

--- lib/middleware/health_monitor_middleware.rb
+++ lib/middleware/health_monitor_middleware.rb
@@ -7,30 +7,30 @@ class HealthMonitorMiddleware
     @uri = uri || "health_monitor"
     @@monitor.name = name
   end
-  
+
   def call(env)
     if env["REQUEST_URI"] =~ /#{@uri}/
 
       request = Rack::Request.new(env)
-      
+
       stime = Time.now
       data = @@monitor.get_status(request.params)
       data[:time] = (Time.now - stime)
 
       if env["REQUEST_URI"] =~ /pingdom/
         status = (data[:status] == :up) ? "OK" : "FAILED"
-        xml_response = "<pingdom_http_custom_check><status>#{status}</status><response_time>#{data[:time]}</response_time></pingdom_http_custom_check>"
+        xml_response = "<pingdom_http_custom_check><status>#{status}</status><response_time>#{data[:time].round(3)}</response_time></pingdom_http_custom_check>"
         [200, { 'Content-Type' => 'application/xml' },  [ xml_response ]]
       else
         [200, { 'Content-Type' => 'application/json' }, [ data.to_json ]]
       end
-    else 
+    else
       @app.call(env)
     end
   end
 
-  def self.add(type, params, &block) 
-    @@monitor.send("add_#{type}", params, &block)  
+  def self.add(type, params, &block)
+    @@monitor.send("add_#{type}", params, &block)
   end
 
 end
